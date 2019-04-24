8c36883a75268ae7b7cb8d715907a86d31e5d539
bgthomas <benthomascodes@gmail.com>
2019-04-24 21:11:37 +0100

    Animation kinda working. Need to incorperate into main game code.

---
+++ core/src/com/magneto/platformer/DinoAnimator.java
@@ -0,0 +1,90 @@
+package com.magneto.platformer;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class DinoAnimator implements ApplicationListener {
+
+    private static final int FRAME_COLS = 24, FRAME_ROWS=1;
+
+    Animation<TextureRegion> walkAnimation;
+    Texture walkSheet;
+    SpriteBatch spriteBatch;
+
+    float stateTime;
+
+    public DinoAnimator(){
+
+
+    }
+    @Override
+    public void create(){
+
+        walkSheet = new Texture(Gdx.files.internal("dinoDoux.png"));
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
+                walkSheet.getWidth() / FRAME_COLS,
+                walkSheet.getHeight() / FRAME_ROWS);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+
+        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
+        // time to 0
+        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+
+        // Get current frame of animation for the current stateTime
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
+        spriteBatch.end();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() { // SpriteBatches and Textures must always be disposed
+        spriteBatch.dispose();
+        walkSheet.dispose();
+    }
+
+
+}
